c577bea7d85a09bdde31edf54eb81533b899fbc1
Corey Farwell <coreyf@rwell.org>
2018-03-30 17:39:26 +0200

    minor performance tweaks to png fuzz target

--- image/read_write_read_png.rs
+++ image/read_write_read_png.rs
@@ -7,27 +7,28 @@ extern crate png;
 
 use png::HasParameters;
 
-fn decode(data: &[u8]) -> Result<(png::OutputInfo, Vec<u8>), png::DecodingError> {
+fn decode(data: &[u8]) -> Result<(png::OutputInfo, Vec<u8>), ()> {
     let decoder = png::Decoder::new(data);
-    let (info, mut reader) = decoder.read_info()?;
+    let (info, mut reader) = decoder.read_info().map_err(|_| ())?;
 
     if info.buffer_size() > 50_000_000 {
-        return Err("Too large".to_string().into());
+        return Err(());
     }
 
-    let mut img_data = vec![0; info.buffer_size()];
-    reader.next_frame(&mut img_data)?;
+    let mut img_data = Vec::with_capacity(info.buffer_size());
+    reader.next_frame(&mut img_data).map_err(|_| ())?;
 
     Ok((info, img_data))
 }
 
-fn encode(info: &png::OutputInfo, data: &[u8]) -> Result<Vec<u8>, png::EncodingError> {
+fn encode(info: &png::OutputInfo, data: &[u8]) -> Result<Vec<u8>, ()> {
     let mut out = Vec::with_capacity(data.len());
+
     {
         let mut encoder = png::Encoder::new(&mut out, info.width, info.height);
         encoder.set(info.color_type).set(info.bit_depth);
-        let mut writer = encoder.write_header()?;
-        writer.write_image_data(&data)?;
+        let mut writer = encoder.write_header().map_err(|_| ())?;
+        writer.write_image_data(&data).map_err(|_| ())?;
     }
 
     Ok(out)
