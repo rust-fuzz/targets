3862930f0f35e892644fdef0fd62398de3bdf12d
Paul Grandperrin <paul.grandperrin@gmail.com>
2018-04-21 16:59:12 +0200

    clean targets before regenerating

--- gen-targets-src.sh
+++ gen-targets-src.sh
@@ -2,6 +2,7 @@
 
 for D in fuzzer-*/; do
 	mkdir -p "${D}/src/bin"
+	rm -f ${D}/src/bin/*.rs
 	cd common
 	cargo run --bin gen-target-src "../targets.txt" "../${D}/template.rs" "../${D}/src/bin"
 	cd ..
