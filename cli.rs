a29e7efe4b605d1059c1e0b95a9e9f6f254d450a
Pascal Hertleif <killercup@gmail.com>
2018-04-26 13:09:42 +0200

    Add simple "did you mean" suggestions for targets
    Copied from clap -- thanks, Kevin!

--- cli.rs
+++ cli.rs
@@ -64,6 +64,7 @@ fn main() {
         for cause in e.causes().skip(1) {
             eprintln!("caused by: {}", cause);
         }
+        ::std::process::exit(1);
     }
 }
 
@@ -78,6 +79,19 @@ fn run() -> Result<(), Error> {
             }
         }
         Run { target, fuzzer } => {
+            let targets = get_targets()?;
+            if targets.iter().find(|x| *x == &target).is_none() {
+                bail!(
+                    "Don't know target `{}`. {}",
+                    target,
+                    if let Some(alt) = did_you_mean(&target, &targets) {
+                        format!("Did you mean `{}`?", alt)
+                    } else {
+                        "".into()
+                    }
+                );
+            }
+
             use Fuzzer::*;
             match fuzzer {
                 Afl => run_afl(&target, None)?,
@@ -348,5 +362,34 @@ impl Fuzzer {
         };
 
         Ok(p)
+    }
+}
+
+/// Produces a string from a given list of possible values which is similar to
+/// the passed in value `v` with a certain confidence.
+/// Thus in a list of possible values like ["foo", "bar"], the value "fop" will yield
+/// `Some("foo")`, whereas "blark" would yield `None`.
+///
+/// Originally from [clap] which is Copyright (c) 2015-2016 Kevin B. Knapp
+///
+/// [clap]: https://github.com/kbknapp/clap-rs/blob/dc7ae65fb784dc355d56f09554f1216b22755c3e/src/suggestions.rs
+pub fn did_you_mean<'a, T: ?Sized, I>(v: &str, possible_values: I) -> Option<&'a str>
+where
+    T: AsRef<str> + 'a,
+    I: IntoIterator<Item = &'a T>,
+{
+    extern crate strsim;
+
+    let mut candidate: Option<(f64, &str)> = None;
+    for pv in possible_values {
+        let confidence = strsim::jaro_winkler(v, pv.as_ref());
+        if confidence > 0.8 && (candidate.is_none() || (candidate.as_ref().unwrap().0 < confidence))
+        {
+            candidate = Some((confidence, pv.as_ref()));
+        }
+    }
+    match candidate {
+        None => None,
+        Some((_, candidate)) => Some(candidate),
     }
 }
