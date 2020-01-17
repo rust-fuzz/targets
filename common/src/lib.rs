750da4fee8f0fe1925155668e3d84590b6316c66
David Cook <divergentdave@gmail.com>
2020-01-16 23:02:43 -0600

    Update svgtypes
    StyleParser was removed from the crate

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -887,23 +887,6 @@ pub fn fuzz_svgtypes_path(data: &[u8]) {
 }
 
 #[inline(always)]
-pub fn fuzz_svgtypes_style(data: &[u8]) {
-    use std::str;
-
-    if let Ok(s) = str::from_utf8(data) {
-        // Must not panic.
-        let mut n = 0;
-        for _ in svgtypes::StyleParser::from(s) {
-            n += 1;
-
-            if n == 1000 {
-                panic!("endless loop");
-            }
-        }
-    }
-}
-
-#[inline(always)]
 pub fn fuzz_svgtypes_transforms(data: &[u8]) {
     use std::str;
 
